a511c41720ffc568260dad97fcfe62706015b20b
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-04-10 16:34:22 +0200

    Change to float

--- operaciones.adb
+++ operaciones.adb
@@ -1,14 +1,22 @@
-with Ada.Text_IO;         use Ada.Text_IO;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Text_IO;       use Ada.Text_IO;
+with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 procedure Operaciones is
-   A, B : Integer;
+   A, B : Float;
 begin
    Put ("Introduce el primer número: ");
    Get (A);
    Put ("Introduce el segundo número: ");
    Get (B);
-   Put_Line ("Suma: " & Integer'Image (A + B));
-   Put_Line ("Resta: " & Integer'Image (A - B));
-   Put_Line ("Multiplicación: " & Integer'Image (A * B));
-   Put_Line ("División: " & Integer'Image (A / B));
+   Put ("Suma: ");
+   Put (A + B, Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
+   Put ("Resta: ");
+   Put (A - B, Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
+   Put ("Multiplicación: ");
+   Put (A * B, Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
+   Put ("División: ");
+   Put (A / B, Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
 end Operaciones;
