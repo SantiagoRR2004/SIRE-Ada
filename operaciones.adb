58885f21379df0a56c62bdf33cdcfb39166d8c58
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-04-10 18:27:43 +0200

    Input validation and division by zero

--- operaciones.adb
+++ operaciones.adb
@@ -2,11 +2,26 @@ with Ada.Text_IO;       use Ada.Text_IO;
 with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 procedure Operaciones is
    A, B : Float;
+   function Leer_Float (Mensaje : String) return Float is
+      Valor : Float;
+   begin
+      loop
+         Put (Mensaje);
+         begin
+            Get (Valor);
+            return Valor;
+         exception
+            when Data_Error =>
+               Put_Line
+                 ("Entrada inválida. Por favor, introduce un número válido.");
+               Skip_Line;
+         end;
+      end loop;
+   end Leer_Float;
+
 begin
-   Put ("Introduce el primer número: ");
-   Get (A);
-   Put ("Introduce el segundo número: ");
-   Get (B);
+   A := Leer_Float ("Introduce el primer número: ");
+   B := Leer_Float ("Introduce el segundo número: ");
    Put ("Suma: ");
    Put (A + B, Fore => 1, Aft => 2, Exp => 0);
    New_Line;
@@ -16,7 +31,11 @@ begin
    Put ("Multiplicación: ");
    Put (A * B, Fore => 1, Aft => 2, Exp => 0);
    New_Line;
-   Put ("División: ");
-   Put (A / B, Fore => 1, Aft => 2, Exp => 0);
-   New_Line;
+   if B /= 0.0 then
+      Put ("División: ");
+      Put (A / B, Fore => 1, Aft => 2, Exp => 0);
+      New_Line;
+   else
+      Put_Line ("Error: División por cero no permitida.");
+   end if;
 end Operaciones;
