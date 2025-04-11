844ba760d6b6be7bfd72f668e4085e4a8a56ec00
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-04-11 10:01:42 +0200

    Write student

--- p5/registro_alumno.adb
+++ p5/registro_alumno.adb
@@ -1,5 +1,8 @@
 with Ada.Text_IO;           use Ada.Text_IO;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Text_IO.Unbounded_IO;
+with Ada.Integer_Text_IO;
+with Ada.Float_Text_IO;
 procedure Registro_Alumno is
     type Alumno is record
         Nombre       : Unbounded_String;
@@ -8,9 +11,12 @@ procedure Registro_Alumno is
     end record;
     Estudiante : Alumno;
 begin
-    Estudiante :=
-       (Nombre       => To_Unbounded_String ("Juan Perez"), Edad => 20,
-        Calificacion => 9.5);
+    Put("Introduce el nombre del estudiante: ");
+    Ada.Text_IO.Unbounded_IO.Get_Line(Estudiante.Nombre);
+    Put("Introduce la edad del estudiante: ");
+    Ada.Integer_Text_IO.Get(Estudiante.Edad);
+    Put("Introduce la calificación del estudiante: ");
+    Ada.Float_Text_IO.Get(Estudiante.Calificacion);
     Put_Line ("Nombre: " & To_String (Estudiante.Nombre));
     Put_Line ("Edad: " & Integer'Image (Estudiante.Edad));
     Put_Line ("Calificaci´on: " & Float'Image (Estudiante.Calificacion));
