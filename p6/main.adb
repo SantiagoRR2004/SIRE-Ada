56e9ea917b9d1d991aec0e85c2733a29b1f3fb3d
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-04-17 16:02:43 +0200

    Improve main

--- p6/main.adb
+++ p6/main.adb
@@ -1,59 +1,89 @@
 with Banco, Cuenta, Ada.Text_IO, Ada.Strings.Unbounded;
 use Ada.Text_IO;
 use Ada.Strings.Unbounded;
+with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 procedure Main is
-   B                  : Banco.Banco_Array;
-   C1, C2             : Cuenta.Cuenta_bancaria;
-   Monto              : Float;
-   Titular1, Titular2 : Unbounded_String;
-   Saldo1, Saldo2     : Float;
-   Total_Banco        : Float;
+   B                                                : Banco.Banco_Array;
+   C1, C2, C3, C4, C5                               : Cuenta.Cuenta_bancaria;
+   Monto                                            : Float;
+   Titular1, Titular2, Titular3, Titular4, Titular5 : Unbounded_String;
 begin
    -- Crear el banco
-   B  := Banco.Crear_Banco;
+   B := Banco.Crear_Banco;
+
+   -- Crear titulares
+   Titular1 := To_Unbounded_String ("Sergio");
+   Titular2 := To_Unbounded_String ("Santiago");
+   Titular3 := To_Unbounded_String ("María");
+   Titular4 := To_Unbounded_String ("Juan");
+   Titular5 := To_Unbounded_String ("Ana");
+
    -- Crear cuentas
-   C1 := Cuenta.Crear (To_Unbounded_String ("Sergio"), 1_000.0);
-   C2 := Cuenta.Crear (To_Unbounded_String ("Santiago"), 500.0);
+   C1 := Cuenta.Crear (Titular1, 1_000.0);
+   C2 := Cuenta.Crear (Titular2, 500.0);
+   C3 := Cuenta.Crear (Titular3, 1_500.0);
+   C4 := Cuenta.Crear (Titular4, 0.5);
+   C5 := Cuenta.Crear (Titular5, 100.0);
+
    -- Agregar cuentas al banco
    Banco.Agregar_Cuenta (B, C1);
    Banco.Agregar_Cuenta (B, C2);
+   Banco.Agregar_Cuenta (B, C3);
+   Banco.Agregar_Cuenta (B, C4);
+   Banco.Agregar_Cuenta (B, C5);
+
+   -- Saldo del banco inicial
+   Put ("Saldo inicial del banco: ");
+   Put (Banco.Consultar_Saldo_Banco (B), Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
+   New_Line;
+
    -- Hacer una transferencia
-   Titular1 := To_Unbounded_String ("Sergio");
-   Titular2 := To_Unbounded_String ("Santiago");
-   Monto    := 200.0;
+   Monto := 200.0;
+   Put ("Saldo de " & To_String (Titular1) & " antes de la transferencia: ");
+   Put (Banco.Consultar_Cuenta (B, Titular1), Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
+   Put ("Saldo de " & To_String (Titular2) & " antes de la transferencia: ");
+   Put (Banco.Consultar_Cuenta (B, Titular2), Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
    Banco.Transferir (B, Titular1, Titular2, Monto);
-   -- Consultar saldo de las cuentas
-   Saldo1      := Banco.Consultar_Cuenta (B, Titular1);
-   Saldo2      := Banco.Consultar_Cuenta (B, Titular2);
-   Total_Banco := Banco.Consultar_Saldo_Banco (B);
-   -- Mostrar resultados
-   Put_Line ("Saldo de " & To_String (Titular1) & ": " & Float'Image (Saldo1));
-   Put_Line ("Saldo de " & To_String (Titular2) & ": " & Float'Image (Saldo2));
-   Put_Line ("Total en el banco: " & Float'Image (Total_Banco));
+   Put ("Saldo de " & To_String (Titular1) & " después de la transferencia: ");
+   Put (Banco.Consultar_Cuenta (B, Titular1), Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
+   Put ("Saldo de " & To_String (Titular2) & " después de la transferencia: ");
+   Put (Banco.Consultar_Cuenta (B, Titular2), Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
+   Put ("Total en el banco: ");
+   Put (Banco.Consultar_Saldo_Banco (B), Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
+   New_Line;
+
    -- Realizar un retiro
    Monto := 300.0;
+   Put ("Saldo de " & To_String (Titular1) & " antes del retiro: ");
+   Put (Banco.Consultar_Cuenta (B, Titular1), Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
    Banco.Retirar (B, Cuenta.Consultar_Titular (C1), Monto);
-   -- Consultar saldo después del retiro
-   Saldo1 := Banco.Consultar_Cuenta (B, Titular1);
-   -- Mostrar saldo después del retiro
-   Put_Line
-     ("Saldo de " & To_String (Titular1) & " después del retiro: " &
-      Float'Image (Saldo1));
+   Put ("Saldo de " & To_String (Titular1) & " después del retiro: ");
+   Put (Banco.Consultar_Cuenta (B, Titular1), Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
+   New_Line;
+
    -- Realizar un depósito
    Monto := 150.0;
+   Put ("Saldo de " & To_String (Titular2) & " antes del depósito: ");
+   Put (Banco.Consultar_Cuenta (B, Titular2), Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
    Banco.Depositar (B, Cuenta.Consultar_Titular (C2), Monto);
-   -- Consultar saldo después del depósito
-   Saldo2 := Banco.Consultar_Cuenta (B, Titular2);
-   -- Mostrar saldo después del depósito
-   Put_Line
-     ("Saldo de " & To_String (Titular2) & " después del depósito: " &
-      Float'Image (Saldo2));
+   Put ("Saldo de " & To_String (Titular2) & " después del depósito: ");
+   Put (Banco.Consultar_Cuenta (B, Titular2), Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
+   New_Line;
+
    -- Consultar saldo total del banco
-   Total_Banco := Banco.Consultar_Saldo_Banco (B);
-   -- Mostrar saldo total del banco
-   Put_Line
-     ("Total en el banco después de las transacciones: " &
-      Float'Image (Total_Banco));
+   Put ("Total en el banco después de las transacciones: ");
+   Put (Banco.Consultar_Saldo_Banco (B), Fore => 1, Aft => 2, Exp => 0);
+   New_Line;
 
    -- Finalizar cuentas
    Banco.Finalizar (B);
