3f8f3087bbee5c8792801bb6ad55b24b781ee65e
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-04-29 17:11:30 +0200

    Refactor buffer instantiation to use aliased type and streamline task execution

--- p7/buffer_limitado_array_3.adb
+++ p7/buffer_limitado_array_3.adb
@@ -1,11 +1,18 @@
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO;      use Ada.Text_IO;
+with Buffer_Interface; use Buffer_Interface;
+with Main;
 
 procedure Buffer_Limitado_Array is
+
    type Buffer_Array is array (1 .. 5) of Integer;
+
    task type Buffer is
+     new IBuffer
+   with
       entry Insertar (D : in Integer);
       entry Extraer (D : out Integer);
    end Buffer;
+
    task body Buffer is
       Dato     : Buffer_Array := (others => 0); -- Buffer inicialmente vacío
       Contador : Integer := 0; -- Contador de elementos en el buffer
@@ -42,28 +49,12 @@ procedure Buffer_Limitado_Array is
          end select;
       end loop;
    end Buffer;
-   B : Buffer; -- Instanciamos el buffer
-   task type Productor;
-   task type Consumidor;
-   task body Productor is
-   begin
-      delay 1.5;
-      for I in 1 .. 3 loop
-         B.Insertar (I); -- Intenta insertar un valor en el buffer
-      end loop;
-   end Productor;
-   task body Consumidor is
-      V : Integer;
-   begin
-      delay 1.5;
-      for I in 1 .. 3 loop
-         B.Extraer (V); -- Intenta extraer un valor del buffer
-      end loop;
-   end Consumidor;
-   P : Productor; -- Instancia de la tarea Productor
-   C : Consumidor; -- Instancia de la tarea Consumidor
+
+   B : aliased Buffer; -- Instanciamos el buffer
+
 begin
    Put_Line ("=== Ejercicio 3.3: Buffer con array ===");
+   Main.runBuffer (B'Access);
    delay
      6.0; -- Para permitir que las tareas Productor y Consumidor trabajen un poco
 end Buffer_Limitado_Array;
